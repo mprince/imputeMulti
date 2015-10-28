6c49c79ef4456ec0a80a4d824e7e7b6a044007bf
Marcel Prince <mprince@users.noreply.github.com>
2015-10-28 15:01:20 -0700

    Intro Rcpp

---
+++ src/supDistC.cpp
@@ -0,0 +1,15 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+double supDistCpp(NumericVector x, NumericVector y) {
+  int size = x.size();
+  NumericVector n(size);
+  for (int i = 0; i < size; i++)
+    n[i] = std::abs(x[i] - y[i]);
+  double result = n[0];
+  for (int i =1; i < size; i++)
+    if (n[i] > result)
+      result = n[i];
+    return result;
+}
