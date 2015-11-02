96731501c5bb65485460033adc8954e5d91ff09a
Alex Whitworth <awhitworth@dandb.com>
2015-11-02 12:18:04 -0800

    ... integermatrix

--- R/multinomial_em.R
+++ R/multinomial_em.R
@@ -68,7 +68,8 @@ multinomial_em <- function(x_y, z_Os_y, enum_comp, n_obs,
       # allocate observed marginal counts proportionally to complete patterns
       # E(x_y| z_Os_y, theta) = \sum_s [E_Xsy_Zy_theta]
       # E_Xsy_Zy_theta = (z_Os_y * theta_y) / b_Os_y
-      comp_ind <- marg_complete_compare(as.matrix(z_Os_y[s, -z_p]), as.matrix(enum_comp[, 1:count_p]),
+      comp_ind <- marg_complete_compare(as.matrix(apply(z_Os_y[s, -z_p], 2, as.integer)),  # fix for Rcpp versions
+                                        as.matrix(apply(enum_comp[, 1:count_p], 2, as.integer)),
                                     marg_to_comp= TRUE) # pattern match to complete
 
       b_Os_y <- sum(enum_comp$theta_y[comp_ind])
