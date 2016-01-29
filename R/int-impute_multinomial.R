2380a05a2e93d136ca5c922cf84de3f970689178
Alex Whitworth <awhitworth@dandb.com>
2016-01-29 14:22:10 -0800

    debug @

--- R/int-impute_multinomial.R
+++ R/int-impute_multinomial.R
@@ -36,7 +36,7 @@ impute_multinomial_all <- function(dat_miss, MLEx_y, p) {
 # convert a factor-vector to an integer vector, where the integers correspond
 # to the levels of the factor.
 fact_to_int <- function(f) {
-  l <- levels(f)
+  l <- c(levels(f), NA)
   return(unlist(sapply(f, function(i) which(i == l))))
 }
 
